fbcadfbbb30df2af76f64a4e79f9f2a7578e4774
sjachvaecaesin <booglyboo544@gmail.com>
2023-09-16 20:47:45 -0400

    created the name and grade programs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        Console.WriteLine();
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (letter == "A" || letter == "F")
+        {
+            Console.WriteLine($"Your grade is an {letter}.");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is a {letter}.");
+        }
+
+        if (grade >= 70)
+        {
+            Console.Write("Congratulations on passing!");
+        }
+        else
+        {
+            Console.Write("You didn't pass the course. Better luck next time.");
+        }
     }
 }
