8141cd53ccba920bb79489bed28882c985a0358a
sjachvaecaesin <booglyboo544@gmail.com>
2023-09-27 14:48:45 -0400

    finished the program, created the Job/Resume class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job first = new Job();
+        Job second = new Job();
+
+        first._company = "Microsoft";
+        second._company = "Apple";
+
+        first._jobTitle = "Software Engineer";
+        second._jobTitle = "IT Specialist";
+
+        first._startYear = 2000;
+        second._startYear = 2020;
+
+        first._endYear = 2019;
+        second._endYear = 2039;
+
+        Resume myResume = new Resume();
+
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(first);
+        myResume._jobs.Add(second);
+
+        myResume.Display();
     }
 }
