e35b9134a05135ac12d7e7a69e16cc0f9e2c7079
sjachvaecaesin <booglyboo544@gmail.com>
2023-11-08 22:29:44 -0500

    created the program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square test1 = new Square("red", 4);
+        shapes.Add(test1);
+
+        Rectangle test2 = new Rectangle("blue", 3, 6);
+        shapes.Add(test2);
+
+        Circle test3 = new Circle("green", 8);
+        shapes.Add(test3);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"color: {shape.GetColor()} | area: {shape.GetArea()}");
+        }
     }
 }
